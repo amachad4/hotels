e01f1d30634a1228a79afd9a1e85169486983ce7
amachad4 <amachad4@masonlive.gmu.edu>
2024-10-28 22:14:04 -0400

    Fix env variable injection order

--- server.js
+++ server.js
@@ -30,15 +30,15 @@ app.get("/hotel/:page.html", (req, res) => {
 
       // Inject environment variables into the HTML before serving it
       const injectedHtml = data.replace(
-        "</head>",
-        `<script>
+        "<head>",
+        `<head>
+        <script>
           window.env = {
             IDENTITY_POOL_ID: "${process.env.IDENTITY_POOL_ID || ""}",
             COGNITO_DOMAIN: "${process.env.COGNITO_DOMAIN || ""}",
             APP_ID: "${process.env.APP_ID || ""}",
           };
-        </script>
-        </head>`
+        </script>`
       );
 
       // Send the modified HTML file
@@ -48,6 +48,47 @@ app.get("/hotel/:page.html", (req, res) => {
     // Return a 404 error if the HTML file doesn't exist
     res.status(404).send("Page not found.");
   }
+});
+
+app.get("/hotel/", (req, res) => {
+  const filePath = path.join(__dirname, "hotel", `index.html`);
+
+  // Check if the requested HTML file exists
+  if (fs.existsSync(filePath)) {
+    // Read the HTML file and inject environment variables
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        res.status(500).send("Error reading the HTML file.");
+        return;
+      }
+
+      // Inject environment variables into the HTML before serving it
+      const injectedHtml = data.replace(
+        "<head>",
+        `<head>
+        <script>
+          window.env = {
+            IDENTITY_POOL_ID: "${process.env.IDENTITY_POOL_ID || ""}",
+            COGNITO_DOMAIN: "${process.env.COGNITO_DOMAIN || ""}",
+            APP_ID: "${process.env.APP_ID || ""}",
+          };
+        </script>`
+      );
+
+      // Send the modified HTML file
+      res.send(injectedHtml);
+    });
+  } else {
+    // Return a 404 error if the HTML file doesn't exist
+    res.status(404).send("Page not found.");
+  }
+});
+
+// All your other route handlers here...
+
+// Catch-all route handler for undefined routes
+app.use((req, res) => {
+  res.status(404).send("404: Page Not Found");
 });
 
 // Start the server
